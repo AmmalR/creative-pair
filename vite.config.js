a7bc7746304b2e5b657e903f9a02b538e25699fd
Ammal Raheem <116118880+AmmalR@users.noreply.github.com>
2025-09-02 23:57:04 +0500

    Deployment 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { progress } from 'framer-motion';
 
 export default defineConfig({
   plugins: [react()],
+  base: progress.env.VITE_BASE_PATH || "/creative-pair" ,
 });
